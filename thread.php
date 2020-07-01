fee7d2e18b7f96fa45d4e1d00f27fcc9bc11da2b
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-07-01 10:53:35 +0900

    sfsf

--- thread.php
+++ thread.php
@@ -1,24 +1,25 @@
 <?php
-    class Thread {
 
-        private  $name;
-        private const THREAD_FILE = "toukou.txt";
+class Thread
+{
+    private $name;
+    private const THREAD_FILE = 'thread.txt';
 
-        public function __construct(string $name)
-        {
-            $this->name = $name;
-        }
-    
-        public function getList()
-        {
-            return file_get_contents(self::THREAD_FILE);
-        }
-    
-        public function post() {
-    
-        }
-    
-        public function delete() {
-    
-        } 
+    public function __construct(string $name)
+    {
+        $this->name = $name;
     }
+
+    public function getList()
+    {
+        return file_get_contents(self::THREAD_FILE);
+    }
+
+    public function post() {
+
+    }
+
+    public function delete() {
+
+    }
+}
