b84b8d82320957bad8aa410d39ba619e438dcf50
tetta <19s032032@gmail.com>
2020-07-08 12:03:44 +0900

    スレッドのsql移行

---
+++ sample.php
@@ -0,0 +1,21 @@
+<?php
+
+$dsn = 'mysql:dbname=school;host=localhost';
+$user = 'root';
+$password = 'password';
+
+try{
+    $dbh = new PDO($dsn, $user, $password);
+
+    $sql = 'select * from thread';
+    foreach ($dbh->query($sql) as $row) {
+        // echo $row['name'].'さんは'.$row['club'].'所属です。<br />';
+        echo '<p>'.$row['name'].'</p>';
+        echo '<p>'.$row['content'].'</p>'
+        echo '<p>'.$row['create_at'].'</p>'
+    }
+}catch (PDOException $e){
+    print('Error:'.$e->getMessage());
+    die();
+}
+
--- thread.php
+++ thread.php
@@ -6,29 +6,48 @@ class Thread
 {
     private $name;
     private const THREAD_FILE = 'toukou.txt';
+    private $dbh;
 
     public function __construct(string $name)
     {
+        //どこでも使用できる
         $this->name = $name;
+        //どのデータベースにアクセスする？
+        $this->dbh = new PDO('mysql:dbname=thread;host=127.0.0.1', 'root', 'password');
+
     }
 
     public function getList()
     {
-        return file_get_contents(self::THREAD_FILE);
+        $res = "";
+        $stmt = $this->dbh->query("SELECT * FROM `thread` WHERE `deleted_at` IS NULL ORDER BY `created_at` ASC");
+        while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+            $created_at = $row["created_at"];
+            $name = $row["name"];
+            $contents = $row["content"];
+            $res .= "<hr>\n";
+            $res .= "<p>投稿日時: ".date("Y/m/d H:i:s", strtotime($created_at))."</p>\n";
+            $res .= "<p>投稿者:".$name."</p>\n";
+            $res .= "<p>内容:</p>";
+            $res .= "<p>".nl2br($contents)."</p>\n";
+        }
+
+        return $res;
     }
 
+    //スレッドの内容をデータベースに追加
     public function post(string $personal_name, string $contents)
     {
-        $data = "<hr>\n";
-        $data = $data."<p>投稿日時: ".date("Y/m/d H:i:s")."</p>\n";
-        $data = $data."<p>投稿者:".$personal_name."</p>\n";
-        $data = $data."<p>内容:</p>\n";
-        $data = $data."<p>".$contents."</p>\n";
-
-        file_put_contents(self::THREAD_FILE, $data, FILE_APPEND);
+        $stmt = $this->dbh->prepare("INSERT INTO `thread` (name, content) VALUE (:name, :content)");
+        //
+        $stmt->bindParam(':name', $personal_name, PDO::PARAM_STR);
+        $stmt->bindParam(':content', $contents, PDO::PARAM_STR);
+        $stmt->execute();
     }
 
     public function delete() {
-            file_put_contents(self::THREAD_FILE, "");
+        $sql = "UPDATE `thread` SET `deleted_at` = NOW()";
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute();
     }
 }
